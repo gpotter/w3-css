c5a8012e966adfae07564bef4a62e1f0bb6e4207
Gordon Potter <gordon.l.potter@gmail.com>
2015-10-14 15:37:30 -0400

    Update w3-css.js

--- w3-css.js
+++ w3-css.js
@@ -1,6 +1,19 @@
 /*Don't forget to add jQuery into your site or page BEFORE this script*/
 $(document).ready(function(){
-  $('navBtn').click(function(){
+ /* $('navBtn').click(function(){
     $('w3-topnav').fadeToggle("fast");
+  });*/
+  /*w3-closenav
+    w3-opennav*/
+  $("w3-opennav").click(function(){
+    $("#main").css('margin-left': '25%');
+    $("w3-sidenav").css('width': '25%', 'display': 'block');
+    $("w3-open").css('display': 'none');
   });
+  $("w3-closenav").click(function(){
+    $("#main")
+    
+  })
+    
+  }
 });
