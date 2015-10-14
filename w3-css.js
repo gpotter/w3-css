6f6b063ccb70ae448fbc1657ef0257f4ca468747
Gordon Potter <gordon.l.potter@gmail.com>
2015-10-14 15:09:05 -0400

    Update w3-css.js

--- w3-css.js
+++ w3-css.js
@@ -1,5 +1,4 @@
-/*Don't forget to add jQuery in your page before this script*/
-
+/*Don't forget to add jQuery into your site or page BEFORE this script*/
 $(document).ready(function(){
   $('navBtn').click(function(){
     $('w3-topnav').fadeToggle("fast");
