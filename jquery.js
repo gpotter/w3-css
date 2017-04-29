3263e5f89b884b3846aa63314ad48a5855e5f9ef
Gordon Potter <gordon.l.potter@gmail.com>
2017-04-29 09:33:11 -0400

    Create jquery.js

---
+++ jquery.js
@@ -0,0 +1,12 @@
+/* jQuery 
+Enable http://w3schools.com/lib/css/w3.css
+styles as oop utilizing standard requests to javascript to enable elements such as Accordion, Tabs.
+From there class names will be
+   
+   //psuedo
+   
+   $('.w3-tab').animate('', 0)
+      $('.MY-tab-class').css{}
+                
+
+*/
